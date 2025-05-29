d607f0715fc6b5dc3de101698182d10a27fc53c6
AMAN KUMAR <ak222760@gmail.com>
2025-05-29 12:12:26 +0530

    Added Post Card and post route

---
+++ components/postCard.vue
@@ -0,0 +1,21 @@
+<template>
+ <div class="rounded-sm bg-yellow-500 p-4 border-solid max-w-96">
+    <h2 class="mb-2 text-2xl font-bold">Title Goes Here</h2>
+    <p class="mb-3 text-gray-500">Preview Text for the card goes here ..........</p>
+    <div class="gap-3 flex">
+      <span 
+        class=" bg-gray-700 px-1 py-2 text-md  rounded-sm text-white"
+      >
+      tag1
+      tag2
+      tag3
+      </span>
+    </div>
+  </div>
+ 
+</template>
+<script setup>
+</script>
+
+<style scoped>
+</style>
--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,7 +1,5 @@
 <template>
   <header>
-    
   </header>
-  <NuxtPage/>
-  
+  <postCard/>
 </template>
